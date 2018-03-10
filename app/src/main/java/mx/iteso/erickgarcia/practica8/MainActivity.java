85bd852a17d221976c426ec07474306014c3caff
ImErick <3r1ck.442@gmail.com>
2018-03-10 11:29:12 -0600

    complete main activity and intent, create header and footer

--- app/src/main/java/mx/iteso/erickgarcia/practica8/MainActivity.java
+++ app/src/main/java/mx/iteso/erickgarcia/practica8/MainActivity.java
@@ -1,13 +1,30 @@
 package mx.iteso.erickgarcia.practica8;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textViewMain;
+    Button buttonMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewMain = (TextView) findViewById(R.id.text_viewMain);
+        buttonMain =  (Button) findViewById(R.id.btn_main);
+
+        buttonMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SharedPreferenceActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/mx/iteso/erickgarcia/practica8/SharedPreferenceActivity.java
@@ -0,0 +1,18 @@
+package mx.iteso.erickgarcia.practica8;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by erickgarcia on 10/03/18
+ */
+
+public class SharedPreferenceActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shared_preference_activity);
+    }
+}
