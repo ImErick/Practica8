7fbb2f60ebf3e907c38bd8ffab1ec9d33e7966eb
ImErick <3r1ck.442@gmail.com>
2018-03-10 15:26:45 -0600

    finish addUserFragment saving the info in Shared Preferences

--- app/src/main/java/mx/iteso/erickgarcia/practica8/FragmentAddUser.java
+++ app/src/main/java/mx/iteso/erickgarcia/practica8/FragmentAddUser.java
@@ -6,16 +6,47 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 /**
  * Created by erickgarcia on 10/03/18
  */
 
 public class FragmentAddUser extends Fragment {
+    EditText editTextName, editTextPhone;
+    Button buttonSaveContact;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_user, container, false);
+    public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_add_user, container, false);
+        editTextName = view.findViewById(R.id.editTextName);
+        editTextPhone = view.findViewById(R.id.editTextPhone);
+        buttonSaveContact = view.findViewById(R.id.btnSaveContact);
+
+        buttonSaveContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HashMap<String, String> users = new HashMap<>();
+                if (editTextName.getText().toString().length() > 0 && editTextPhone.getText().toString().length() > 0) { // no esta vacio
+                    users.put(editTextName.getText().toString(), editTextPhone.getText().toString());
+                    if (new SharedPreferenceMethod(getActivity()).addSharedPreferences("user", users)) {
+                        Toast.makeText(getActivity(), "user saved!", Toast.LENGTH_SHORT).show();
+                        editTextName.setText("");
+                        editTextPhone.setText("");
+                        editTextName.requestFocus();
+                    } else
+                        // regreso falso the addSharedPreferences
+                        Toast.makeText(getActivity(), "unable to save the user", Toast.LENGTH_SHORT).show();
+                } else
+                    // la entrada esta vacia o nula
+                    Toast.makeText(getActivity(), "unable to save user or phone number", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 }
---
+++ app/src/main/java/mx/iteso/erickgarcia/practica8/SharedPreferenceMethod.java
@@ -0,0 +1,38 @@
+package mx.iteso.erickgarcia.practica8;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.Map;
+
+/**
+ * Created by erickgarcia on 10/03/18
+ */
+
+public class SharedPreferenceMethod {
+    private Activity activity;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
+    public SharedPreferenceMethod(Activity activity) {
+        this.activity = activity;
+    }
+
+    public boolean addSharedPreferences(String keyShared, Map<String, String> map) {
+        boolean saved = true;
+        sharedPreferences = activity.getSharedPreferences(keyShared, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        for (Map.Entry<String, String> entry: map.entrySet()) {
+            editor.putString(entry.getKey(), entry.getValue());
+            if (editor.commit())
+                continue;
+            else {
+                saved = false; // no guardo
+                break;
+            }
+        }
+        return saved;
+    }
+}
